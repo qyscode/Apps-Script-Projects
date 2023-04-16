da8a53929b8fc4f44c9a4ddaee014a9d2f3b4d03
qyscode <96786782+qyscode@users.noreply.github.com>
2023-04-16 21:11:36 +0800

    Create Automated Email Report with PDF ZIP and Excel.js
    Creates an email that is generated automatically with Sheets and PDFs in a folder attached as an Excel and ZIP file respectively. Removes the need for user to manually download, upload, write the email and update the dates. Creates a copy of the ZIP file sent within the Drive folder where the PDFs were extracted.

---
+++ Automated Email Report with PDF ZIP and Excel.js
@@ -0,0 +1,62 @@
+// Return the current date in currentDate
+const date = new Date();
+let day = date.getDate();
+if (day < 10) {
+  day = "0" + day;
+  }
+let month = date.getMonth() + 1;
+if (month < 10) {
+  month = "0" + month;
+}
+let year = date.getFullYear();
+
+let currentDate = `${day}/${month}/${year}`;
+var folderDateName = `${year}_${month}_${day}`;
+
+function sendEmail() {
+
+  // Define email parameters
+  var recipient = "RECIPIENT HERE";
+  var subject = `Report (${currentDate})`;
+  var body = "Hi all,\n\nAttached are the reports for today.\n\nRegards,\nName\n\n\n";
+
+  // Extract Excel file
+  var fileId = "GOOGLE SHEETS FILE ID HERE";
+  var excel_file = UrlFetchApp.fetch(
+    "https://docs.google.com/feeds/download/spreadsheets/Export?key=" + fileId + "&exportFormat=xlsx",
+    {
+      "headers": {Authorization: "Bearer " + ScriptApp.getOAuthToken()},
+      "muteHttpExceptions": true
+    }
+  ).getBlob().setName(`Excel Report (${currentDate})` + ".xlsx")
+
+  // Extract ZIP/PDF
+  let zip_file_name = 'Reports for '+folderDateName+'.zip'
+  // This line below searches for a folder with the format YYYY_MM_DD within Drive. For a specific, fixed folder, getFolderById may be preferred.
+  const folderIterator = DriveApp.getRootFolder().getFoldersByName(folderDateName);
+  var folder = folderIterator.next();
+
+  var zipped = Utilities.zip(getBlobs(folder, ''), zip_file_name);
+  var zip_id = folder.createFile(zipped).getId();
+  var zip_file = DriveApp.getFileById(zip_id);
+
+  function getBlobs(rootFolder, path) {
+    var blobs = [];
+    var files = rootFolder.getFiles();
+    while (files.hasNext()) {
+      var file = files.next().getBlob();
+      file.setName(path+file.getName());
+      blobs.push(file);
+    }
+    return blobs;
+  }
+
+// Send email
+MailApp.sendEmail(recipient, subject, body, {name:"CUSTOM NAME, IF YOU WANT",attachments: [excel_file, zip_file]})}
+
+
+
+
+
+
+
