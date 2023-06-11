b6c51614163fd9946d3fd29d7905735f0845242a
qyscode <96786782+qyscode@users.noreply.github.com>
2023-06-12 02:28:22 +0800

    Update Automated Email Report with ZIP and Excel.js

--- Automated Email Report with PDF ZIP and Excel.js
+++ Automated Email Report with PDF ZIP and Excel.js
@@ -1,3 +1,5 @@
+let compilation_start = new Date();
+
 // Return the current date in currentDate
 const date = new Date();
 let day = date.getDate();
@@ -53,3 +55,47 @@ function sendEmail() {
 
 // Send email
 MailApp.sendEmail(recipient, subject, body, {name:"OPTIONAL CUSTOM SENDER NAME",attachments: [excel_file, zip_file]})}
+
+let compilation_end = new Date();
+let time = compilation_end - compilation_start;
+Logger.log("compilation time: " + time + "ms");
+
+function sortANDrename() {
+  let renamingstart = new Date();
+
+  // correct workbook name "Daily Report | MTH 2023"
+  let actual_numeric_month = date.getMonth();
+  const monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  const actual_month_name_three = monthNames[actual_numeric_month];
+  const workbookName = `Daily Report | ${actual_month_name_three} 2023`;
+  console.log(`Sheet name is: ${workbookName}`);
+  const sheet = SpreadsheetApp.openById(fileId).getSheetByName('workbookName');
+
+
+  // SORT FROM first S/N of the day
+  var firstSNoftheday = `${folderDateName}_1`
+  var textFinderSORT = sheet.createTextFinder(firstSNoftheday).findNext(); // Finds first instance of file name in sheet
+  var rowSORT = textFinderSORT.getRow();
+  //var colSORT = textFinderSORT.getColumn(); // Column is not needed
+  var range = sheet.getRange(`A${rowSORT}:W`);
+  range.sort({column: 2, ascending: true}); // Sorts "range" by column B
+
+  // For each file in the folder, get the name, search for the name in the excel sheet, look at the TITLE on the same row as the matching serial number, rename the file to that title.
+  const targetFolderPDF = DriveApp.getFolderById("ID HERE"); // Folder containing files
+  const folderIterator = targetFolderPDF.getFoldersByName(folderDateName); // FIND CURRENT DATE FOLDER BASED ON NAME
+  var file_iterator = folderIterator.next().getFiles(); // 
+  while (file_iterator.hasNext()) {
+    const pdfFile = file_iterator.next();  // SELECTS THE NEXT FILE
+    var pdfFileName = pdfFile.getName();
+    var textFinder = sheet.createTextFinder(pdfFileName).findNext(); // Finds first instance of PDF file name in sheet
+    var row = textFinder.getRow();
+    //var col = textFinder.getColumn();
+    var title_of_pdf = sheet.getRange(int,int,int,int).getValues();
+    pdfFile.setName(title_of_pdf)
+  }
+
+  let renamingend = new Date();
+  let renamingtime = renamingend - renamingstart;
+  Logger.log("Sort and renaming time: " + renamingtime + "ms");
+}
+
