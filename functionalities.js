bfd50964ac709b91f27816a4070f2dff4f2eb146
qyscode <96786782+qyscode@users.noreply.github.com>
2023-09-17 21:26:19 +0800

    Create functionalities.js
    Compiled list

---
+++ functionalities.js
@@ -0,0 +1,31 @@
+// Log run time of program
+let runStart = new Date();
+let runEnd = new Date();
+let runTime = runEnd - runStart;
+Logger.log("Excel time: " + runTime + "ms");
+
+// Duplicate a worksheet within a Sheet
+const ss = SpreadsheetApp.openById("ss-id") // spreadsheet
+const ws = ss.getSheetByName("ws-name"); // worksheet
+const os = ws.copyTo(ss).setName("os-name") // output sheet
+
+// Search for keyword among range to last row
+const sourceSheet = ss.getSheetByName(ws-name);
+const relevantRange = sourceSheet.getRange(`A5:A`+ sourceSheet.getLastRow());
+const relevantCells = relevantRange.createTextFinder("keyword").findAll(); // Returns a Range[] data type
+
+// Dates
+// let dateFormat = `${day}/${month}/${year}`;
+const date = new Date();
+let day = double_Digit_(date.getDate());
+let month = double_Digit_(date.getMonth() + 1); // JS counts month from 0
+let year = date.getFullYear();
+function double_Digit_(number) {
+    if (number < 10) {
+        number = "0" + number;
+    }
+    return number
+}
+
+// Misc
+// `${worksheetName.slice(10)}`
