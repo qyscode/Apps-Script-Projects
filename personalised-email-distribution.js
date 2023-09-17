756c987d71d5c4fb3a7d3a2ed243462a6dbaa49f
qyscode <96786782+qyscode@users.noreply.github.com>
2023-09-17 21:49:39 +0800

    Update, simplify personalised-email-distribution.js

--- personalised-email-distribution.js
+++ personalised-email-distribution.js
@@ -1,13 +1,9 @@
 function senderMail() {
-    const id = 'ID GOES HERE'; // Update Sheets workbook id here
-    const sheet = SpreadsheetApp.openById(id).getSheetByName('messages'); // worksheet must be named 'messages'
+    const sheet = SpreadsheetApp.openById(id).getSheetByName('ws-name');
     const data = sheet.getDataRange().getValues();
     const users = data.slice(1);
     users.forEach(user =>{
       const email = user[2]; // emails from 3rd column
-      const message = `Message Goes Here`;
-      const subject = `Subject Goes Here`;
       GmailApp.sendEmail(email,subject,message);
     })
   }
-  
